2eff6cf531d67b85f47c80b34d0655e299c9335d
Naman Lakhani <nlakhani@amgen.com>
2020-04-30 12:48:26 +0530

    Initial commit for SBA FSD

---
+++ projectmanager-server/src/test/java/com/namanlakhani/projectmanager/ProjectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namanlakhani.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
